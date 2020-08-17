d8404a0829255a86c54a57255d76aa4fcce4e541
Eric Crescioni <ecam900@gmail.com>
2020-08-17 09:42:31 -0400

    Boilerplates

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,41 @@
-import '../styles/globals.css'
+import { ThemeProvider } from 'emotion-theming';
+import { Global, css } from '@emotion/core';
+import theme from '../styles/theme';
+import Head from 'next/head';
+import { CSSReset } from '@chakra-ui/core';
+
+const GlobalStyle = ({ children }) => {
+  return (
+    <>
+      <CSSReset />
+      <Global
+        styles={css`
+          html {
+            min-width: 360px;
+            scroll-behavior: smooth;
+          }
+          #__next {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+          }
+        `}
+      />
+    </>
+  );
+};
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Head>
+        <title>Recao Food Project</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
